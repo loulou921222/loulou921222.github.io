443a1dd4909c0c3d877e8f3cffa2fb58d6f2c494
loulou921222 <loulou921222@gmail.com>
2022-07-03 00:09:14 +0100

    game start test

--- play/js.js
+++ play/js.js
@@ -1,3 +1,8 @@
+var ws;
+var playerCount = 0;
+var players = []
+var permissionLevel = 0;
+
 function connect() {
    var username = $('#username').val();
    var IP = $('#IP').val();
@@ -5,13 +10,9 @@ function connect() {
    
    if ("WebSocket" in window) {
       $("#connectmenudiv").hide();
-      var playerCount = 0;
-      var players = []
-      var permissionLevel = 0;
-
       try {
          // open websocket
-         var ws = new WebSocket(`ws://${IP}:${port}/CTD`);
+         ws = new WebSocket(`ws://${IP}:${port}/CTD`);
          
          ws.onopen = function() {
             $("#playerlistdiv").show();
@@ -58,6 +59,10 @@ function connect() {
          ws.onclose = function() { 
             // websocket is closed.
             alert("Disconnected");
+            ws = undefined;
+            playerCount = 0;
+            players = []
+            permissionLevel = 0;
             $("#playerlistdiv").hide();
             $(".usernamediv").hide();
             $("#connectmenudiv").show();
@@ -65,6 +70,10 @@ function connect() {
       }
       catch(e) {
          alert("Invalid IP or port");
+         ws = undefined;
+         playerCount = 0;
+         players = []
+         permissionLevel = 0;
          $("#playerlistdiv").hide();
          $(".usernamediv").hide();
          $("#connectmenudiv").show();
@@ -76,4 +85,13 @@ function connect() {
 
 function helptext() {
    alert("all players type out a string of characters, let's say player 1 types out \"bananA.\"\n\nplayer 2 sees my message and has to explain it to player 3 in a voice chat or just next to them\n\nplayer 3 has to type out the same string for the game to continue\n\nbe careful! someone might type in \"a in all caps\" or \"period question mark\" or something like that!");
+};
+
+function startbtnclick() {
+   if (playerCount < 3) {
+      alert("3 or more players are required to start!");
+   }
+   else {
+      ws.send("requestStart null");
+   }
 };
